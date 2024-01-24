4cde62f318c8ff4cdce2dcbf7030f98e4fe0400e
seungryeol <opop0421@naver.com>
2024-01-24 22:25:08 +0900

    [fix] prisma 시간 설정

--- src/client.ts
+++ src/client.ts
@@ -11,6 +11,7 @@ declare const global: CustomNodeJsGlobal;
 
 const prisma = global.prisma || new PrismaClient(); // 이미 전역 객체에 prisma 속성이 있다면 그대로 사용, 아니라면 새롭게 선언
 
+
 if (config.env === 'development') global.prisma = prisma; //  다른 모듈에서도 동일한 Prisma Client 인스턴스를 공유할 수 있도록 함.
 
 export default prisma;

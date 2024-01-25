17c10a91f4facd67e0ee84f34dc36377c2b4e2dd
seungryeol <opop0421@naver.com>
2024-01-26 02:53:02 +0900

    [fix] 댓글 조회 수정

--- green.Dockerfile
+++ green.Dockerfile
@@ -1,4 +1,4 @@
-FROM node:20.0.0 as green
+FROM node:18.6.0 as green
 
 ENV TZ=Asia/Seoul
 
--- src/modules/challengeScheduler.ts
+++ src/modules/challengeScheduler.ts
@@ -4,42 +4,53 @@ import { challengeDao } from '../dao/index.js';
 import { ChallengeAllInformation, ChallengeAllInformationCustom } from '../interfaces/challenge.interface.js';
 
 
-export const challengeDepositCalculateScheduler =  () => {
- 
-    schedule.scheduleJob('*/10 * * * * *', async function () {       // UTC시간 기준 9시간 차이로 새벽 12시 의미
+export const challengeDepositCalculateScheduler = () => {
 
-      const challegeData = await challengeDao.selectAllChallengeInformation();
+  schedule.scheduleJob('*/10 * * * * *', async function () {       // UTC시간 기준 9시간 차이로 새벽 12시 의미
+
+    const challegeData = await challengeDao.selectAllChallengeInformation();
 
 
-      const targetCount = 2;
+    const targetCount = 7;
 
-      const transformedData: ChallengeAllInformationCustom = challegeData.reduce((acc, item) => {
-        const { challenge_id, deposit, challengeDayCount } = item;
-      
-        if (!acc[challenge_id]) {
-          acc[challenge_id] = {
-            challenge_id,
-            deposit,
-            challengeDayCount,
-            deductions: []
-          };
-        }
-      
-        acc[challenge_id].deductions.push({
-          start_count: item.start_count,
-          end_count: item.end_count,
-          deduction_rate: item.deduction_rate
-        });
-      
-        return acc;
-      }, {} as ChallengeAllInformationCustom);
-      
-      console.log(transformedData['1'].deductions);
+    const transformedData: ChallengeAllInformationCustom = challegeData.reduce((acc, item) => {
+      const { challenge_id, deposit, challengeDayCount } = item;
 
-    })
+      if (!acc[challenge_id]) {
+        acc[challenge_id] = {
+          challenge_id,
+          deposit,
+          challengeDayCount,
+          deductions: []
+        };
+      }
+
+      acc[challenge_id].deductions.push({
+        start_count: item.start_count,
+        end_count: item.end_count,
+        deduction_rate: item.deduction_rate
+      });
+
+      return acc;
+    }, {} as ChallengeAllInformationCustom);
+
+  
+    const targetDeduction = transformedData['1'].deductions.find(({ start_count, end_count }) => targetCount >= start_count && targetCount <= end_count);
+
+    if (targetDeduction) {
+      const { start_count, end_count, deduction_rate } = targetDeduction;
+      console.log(`Target Count: ${targetCount}, Deduction Rate: ${deduction_rate}, Range: ${start_count} ~ ${end_count}`);
+    } else {
+      console.log(`No matching range for Target Count: ${targetCount}`);
+    }
+
+
+
+
+  })
 }
 
 
 
 
-  
+

5e5cc860501d1debf8e1b013604378108df21032
seungryeol <opop0421@naver.com>
2024-01-26 03:17:59 +0900

    [fix] 댓글 조회 수정

--- src/modules/challengeScheduler.ts
+++ src/modules/challengeScheduler.ts
@@ -8,49 +8,71 @@ export const challengeDepositCalculateScheduler = () => {
 
   schedule.scheduleJob('*/10 * * * * *', async function () {       // UTC시간 기준 9시간 차이로 새벽 12시 의미
 
-    const challegeData = await challengeDao.selectAllChallengeInformation();
+    const challengeData = await challengeDao.selectAllChallengeInformation();
+
 
 
     const targetCount = 7;
 
-    const transformedData: ChallengeAllInformationCustom = challegeData.reduce((acc, item) => {
-      const { challenge_id, deposit, challengeDayCount } = item;
+    const sortedChallengeData = sortChallengeData(challengeData)
 
-      if (!acc[challenge_id]) {
-        acc[challenge_id] = {
-          challenge_id,
-          deposit,
-          challengeDayCount,
-          deductions: []
-        };
-      }
+    const caculateDepositResult = calculateDeposit(sortedChallengeData, targetCount, 1);
 
-      acc[challenge_id].deductions.push({
-        start_count: item.start_count,
-        end_count: item.end_count,
-        deduction_rate: item.deduction_rate
-      });
-
-      return acc;
-    }, {} as ChallengeAllInformationCustom);
 
   
-    const targetDeduction = transformedData['1'].deductions.find(({ start_count, end_count }) => targetCount >= start_count && targetCount <= end_count);
-
-    if (targetDeduction) {
-      const { start_count, end_count, deduction_rate } = targetDeduction;
-      console.log(`Target Count: ${targetCount}, Deduction Rate: ${deduction_rate}, Range: ${start_count} ~ ${end_count}`);
-      console.log(transformedData['1'].deposit * (100 - deduction_rate)/100)
-    } else {
-      console.log(`No matching range for Target Count: ${targetCount}`);
-    }
-
-
 
 
   })
 }
 
+
+const calculateDeposit = (
+  sortedChallengeData: ChallengeAllInformationCustom,
+  targetCount: number,
+  key: number
+
+) => {
+  const targetDeduction = sortedChallengeData[key].deductions.find(({ start_count, end_count }) => targetCount >= start_count && targetCount <= end_count);
+
+  if (targetDeduction) {
+    const { start_count, end_count, deduction_rate } = targetDeduction;
+    console.log(`Target Count: ${targetCount}, Deduction Rate: ${deduction_rate}, Range: ${start_count} ~ ${end_count}`);
+    console.log(sortedChallengeData[key].deposit * (100 - deduction_rate)/100)
+  } else {
+    console.log(`No matching range for Target Count: ${targetCount}`);
+  }
+}
+
+
+
+const sortChallengeData = (
+  challengeData: ChallengeAllInformation[]
+) => {
+
+  const sortedChallengeData = challengeData.reduce((acc, item) => {
+    const { challenge_id, deposit, challengeDayCount } = item;
+
+    if (!acc[challenge_id]) {
+      acc[challenge_id] = {
+        challenge_id,
+        deposit,
+        challengeDayCount,
+        deductions: []
+      };
+    }
+
+    acc[challenge_id].deductions.push({
+      start_count: item.start_count,
+      end_count: item.end_count,
+      deduction_rate: item.deduction_rate
+    });
+
+    return acc;
+  }, {} as ChallengeAllInformationCustom);
+
+  return sortedChallengeData
+
+}
 
 
 

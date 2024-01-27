c73afbc71901554ee9a28995d45bee1f32c040b9
seungryeol <opop0421@naver.com>
2024-01-27 14:35:51 +0900

    [fix] 에러처리 활성화

--- src/services/schedule.service.ts
+++ src/services/schedule.service.ts
@@ -1,9 +1,5 @@
 import httpStatus from 'http-status';
 import ApiError from '../utils/ApiError.js';
-import { affiliationDao, challengeDao, challengeDayDao, userChallengeDao, userDao, userTemplateDao } from '../dao/index.js';
-
-import { signChallengeComplete, signTodayTemplateStatusCalculation, sortUserTemplate } from '../utils/challenge.js';
-import { sortCallendarDateBadge } from '../utils/record.js';
 import { challengeDepositCalculateScheduler } from '../modules/challengeScheduler.js';
 
 
